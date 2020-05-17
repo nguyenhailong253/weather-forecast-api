589b7e3fb5b24b636ecb17211b27b097392ac024
Long Nguyen <nguyenhailong253@gmail.com>
2020-05-17 15:12:03 +1000

    Add support for multiple cities existing in DB

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const express = require('express'); // import express
 const app = express(); // initialize express
 const fakeWeatherData = require('./data'); // import fake data
 
-app.get('/', function(req, res) {
-  // if no city param exits
-  if (!req.query.city) {
-    res.send({
-      'status': 'error',
-      'message': 'Please enter a city name',
-    })
-  } else {
-    res.send(fakeWeatherData[0])
+app.get('/weather', function(req, res) {
+  let cityName = req.query.city.toLowerCase();
+
+  for (let i = 0; i < fakeWeatherData.length; i++) {
+      // if no city param exits
+      if (!cityName) {
+        return res.send({
+          'status': 'error',
+          'message': 'Please enter a city name',
+        });
+      } else if (cityName == fakeWeatherData[i].city.toLowerCase()) {
+        return res.send(fakeWeatherData[i]);
+      }
   }
+  res.send({
+    'status': 'error',
+    'message': 'This city is not in our database',
+  })
 })
 
 // Listen on port 3333
