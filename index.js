2e1f45a0651de99378973619d4c3e1888f5be1b8
Long Nguyen <nguyenhailong253@gmail.com>
2020-05-17 15:05:01 +1000

    Add GET request handler

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express'); // import express
 const app = express(); // initialize express
+const fakeWeatherData = require('./data'); // import fake data
 
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log('listening on port 3000...');
+app.get('/', function(req, res) {
+  // if no city param exits
+  if (!req.query.city) {
+    res.send({
+      'status': 'error',
+      'message': 'Please enter a city name',
+    })
+  } else {
+    res.send(fakeWeatherData[0])
+  }
+})
+
+// Listen on port 3333
+app.listen(3333, function() {
+  console.log('listening on port 3333...');
 })
